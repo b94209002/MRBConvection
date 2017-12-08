eeb3e96c76b50c0ad6dca8660620d8e3599b3b76
Kan Bok-Hua <mhc431@nyu.edu>
2017-12-08 00:31:17 -0500

    test for rotating and new initial

--- probin.3d.rb
+++ probin.3d.rb
@@ -5,7 +5,7 @@
   rt_xfrontw  = 1
   rt_den_1   = 1.5e7
   rt_den_2   = 8.44407300e+06
-  rt_pertamp = 1.0
+  rt_pertamp = 0.01
   rt_nfreq   = 10
   rt_graddenerr = 1e+6
 
@@ -17,6 +17,6 @@
   rb_m0 = 0.
   rb_mh = -5.222e-1
   rb_bv = 6.958e-1
-  rb_omega = 1.0e-2
+  rb_omega = 0.0
 
  /
