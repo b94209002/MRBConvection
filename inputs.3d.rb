37d9195c04b1a508ee1841f285aa241b34e78aec
Kan Bok-Hua <mhc431@nyu.edu>
2018-01-29 01:29:13 -0500

    correct version of Ekman layer boundary layer

--- inputs.3d.rb
+++ inputs.3d.rb
@@ -3,11 +3,11 @@
 # INPUTS.3D.EULER
 #*******************************************************************************
 
-max_step 		= 150
+max_step 		= 1200
 
 stop_time 		= 120.0
 
-amr.n_cell              = 128 128 64
+amr.n_cell              = 32 32 128
 
 amr.max_level           = 0
 
@@ -20,29 +20,29 @@ amr.v                   = 1
 
 amr.check_int		= 10000 
 
-amr.plot_int		= 5
+amr.plot_int		= 30
 
 ns.cfl                  = 0.7  # CFL number used to set dt
 
 ns.init_shrink          = 1.0  # factor which multiplies the very first time step
 
-ns.vel_visc_coef        = 0.001 
-ns.scal_diff_coefs      = 0.0014 0.0014
+ns.vel_visc_coef        = 0.1 
+ns.scal_diff_coefs      = 0.1 0.1
 ns.do_trac2             = 1
 
 amr.probin_file 	= probin.3d.rb
 
 geometry.coord_sys   =  0
 
-geometry.prob_lo     =  0. 0. 0.
-geometry.prob_hi     =  2. 2. 1.
+geometry.prob_lo     =  -0.25 -0.25 -1.
+geometry.prob_hi     =  0.25 0.25 1.
 
 geometry.is_periodic =  1 1 0
 
 ns.gravity = 0.0
 
-ns.lo_bc             = 0 0 4
-ns.hi_bc             = 0 0 4
+ns.lo_bc             = 0 0 5
+ns.hi_bc             = 0 0 5
 
 # 0 = Interior/Periodic  3 = Symmetry
 # 1 = Inflow             4 = SlipWall
@@ -57,3 +57,6 @@ ns.liquid_interval      = 1
 amr.derive_plot_vars    = energy liquid_water
 
 proj.v=1
+proj.proj_tol = 1.e-12
+proj.proj_abs_tol = 1.e-14
+mg.v=2
